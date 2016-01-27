a5fa5f254b1ff3b3efde1e3efdc9c7eabe196a23
abraxasrex <jbreasor@gmail.com>
2016-01-27 13:02:33 -0800

    first commit; script to be moved from index page

--- copdata/client/exportapedia.js
+++ copdata/client/exportapedia.js
@@ -2,20 +2,6 @@ Session.set('resultsToShow', 'nothing happened');
 
 if(Meteor.isClient){
 
-    
+
 
 };
-
-//Template.body.helpers({
-//  'showMeResults': function(){
-  //  Meteor.call('fetchWorldBank', function(err, results) {
-  //      if (!err) {
-  //        console.log(JSON.stringify(results.content));
-  //        console.log(results.content);
-  //        return results.content;
-//        } else {
-//          console.log("couldn't fetch");
-//        }
-  //    });
-//  }
-//})
