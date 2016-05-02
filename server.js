df345b829720360eee0bc4d9c61484ffb4543c53
abraxasrex <jbreasor@gmail.com>
2016-05-02 12:11:04 -0700

    pkg update

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 if(Meteor.isServer){
 
-Meteor.methods({
+  Meteor.methods({
 
-  'serverCall': function(url){
-  var method = 'GET';
-      var options = {
-        crossDomain: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'POST, PUT, DELETE, GET, OPTIONS',
-              'Access-Control-Request-Method': '*',
-              'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-              'content-type': 'application/json'
+    'serverCall': function(url){
+    var method = 'GET';
+        var options = {
+          crossDomain: true,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, PUT, DELETE, GET, OPTIONS',
+                'Access-Control-Request-Method': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                'content-type': 'application/json'
+          }
         }
-      }
-    this.unblock();
-  return Meteor.http.call(method, url, options);
-}
+      this.unblock();
+    return Meteor.http.call(method, url, options);
+  }
 
 
-});
+  });
 
 };
